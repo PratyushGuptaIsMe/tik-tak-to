b69339cac736c4959787bd093e7c7ed11203b5f6
PratyushGuptaIsMe <pratyushngupta@gmail.com>
2025-08-30 20:37:30 -0500

    Logic

--- script.js
+++ script.js
@@ -10,31 +10,29 @@ let b6 = document.getElementById("b6");
 let b7 = document.getElementById("b7");
 let b8 = document.getElementById("b8");
 let b9 = document.getElementById("b9");
-
 let boxes = [b1, b2, b3, b4, b5, b6, b7, b8, b9];
-boxes.forEach((e) => {
-    e.defVeryUniqueBooleanVariableThatHasNeverBeenUsedBefore = true;
-})
-boxes.forEach((e) => {
-    e.addEventListener("click", () => {
-        if(e.defVeryUniqueBooleanVariableThatHasNeverBeenUsedBefore){
-            clickBox(e);
-        }
-    });
-})
 
 let A = X.cloneNode(true);
+A.symbol = "x";
 let B = O.cloneNode(true);
+B.symbol = "o";
 let C = X.cloneNode(true);
+C.symbol = "x";
 let D = O.cloneNode(true);
+D.symbol = "o";
 let E = X.cloneNode(true);
+E.symbol = "x";
 let F = O.cloneNode(true);
+F.symbol = "o";
 let G = X.cloneNode(true);
+G.symbol = "x";
 let H = O.cloneNode(true);
+H.symbol = "o";
 let I = X.cloneNode(true);
+I.symbol = "x";
 let symbols = [A, B, C, D, E, F, G, H, I];
 let index = 0;
-a();
+
 function RESET_IMAGES(){
     X = new Image(80, 80);
     O = new Image(80, 80);
@@ -46,9 +44,78 @@ function clickBox(e){
     e.appendChild(symbols[index]);
     index++;
     e.defVeryUniqueBooleanVariableThatHasNeverBeenUsedBefore = false;
+    checkWinOrLose();
 }
 
-function a(){
-    console.log(index);
-    requestAnimationFrame(a);
+function checkWinOrLose(){
+    try{
+        if( b1.children[0].symbol === b2.children[0].symbol &&
+            b2.children[0].symbol === b3.children[0].symbol &&
+            b3.children[0].symbol === b1.children[0].symbol
+        ){
+            PlayerWon(b1.children[0].symbol);
+        }
+        if( b4.children[0].symbol === b5.children[0].symbol &&
+            b5.children[0].symbol === b6.children[0].symbol &&
+            b6.children[0].symbol === b4.children[0].symbol
+        ){
+            PlayerWon(b4.children[0].symbol);
+        }
+        if( b7.children[0].symbol === b8.children[0].symbol &&
+            b8.children[0].symbol === b9.children[0].symbol &&
+            b9.children[0].symbol === b7.children[0].symbol
+        ){
+            PlayerWon(b7.children[0].symbol);
+        }
+        if( b1.children[0].symbol === b4.children[0].symbol &&
+            b4.children[0].symbol === b7.children[0].symbol &&
+            b7.children[0].symbol === b1.children[0].symbol
+        ){
+            PlayerWon(b7.children[0].symbol);
+        }
+        if( b2.children[0].symbol === b5.children[0].symbol &&
+            b5.children[0].symbol === b8.children[0].symbol &&
+            b8.children[0].symbol === b2.children[0].symbol
+        ){
+            PlayerWon(b2.children[0].symbol);
+        }
+        if( b3.children[0].symbol === b6.children[0].symbol &&
+            b6.children[0].symbol === b9.children[0].symbol &&
+            b9.children[0].symbol === b3.children[0].symbol
+        ){
+            PlayerWon(b6.children[0].symbol);
+        }
+        if( b1.children[0].symbol === b5.children[0].symbol &&
+            b5.children[0].symbol === b9.children[0].symbol &&
+            b9.children[0].symbol === b1.children[0].symbol
+        ){
+            PlayerWon(b9.children[0].symbol);
+        }
+        if( b7.children[0].symbol === b3.children[0].symbol &&
+            b5.children[0].symbol === b7.children[0].symbol &&
+            b3.children[0].symbol === b5.children[0].symbol
+        ){
+            PlayerWon(b7.children[0].symbol);
+        }
+    }
+    catch(e){
+
+    }
 }
+
+function PlayerWon(whoWon){
+    if(whoWon === "x"){
+        console.log("X wins");
+    }else if(whoWon === "o"){
+        console.log("O wins");
+    }
+}
+
+boxes.forEach((e) => {
+    e.defVeryUniqueBooleanVariableThatHasNeverBeenUsedBefore = true;
+    e.addEventListener("click", () => {
+        if(e.defVeryUniqueBooleanVariableThatHasNeverBeenUsedBefore){
+            clickBox(e);
+        }
+    });
+})
